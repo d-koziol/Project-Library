3717ef2a498a7e22e723813c0f0272cf24afbc92
damcio <d.p.koziol@gmail.com>
2023-03-05 01:07:42 +0100

    Book class added & addBookToLibrary fonction added

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+class Book {
+  constructor(title, author, pagesNum, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pagesNum = pagesNum;
+    this.haveRead = haveRead;
+  }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, numOfPages, haveRead) {
+  const myBook = new Book(title, author, numOfPages, haveRead);
+  myLibrary.push(myBook);
 }

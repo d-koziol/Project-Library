e69c9a5939409306eacb1798b1960bd399ae76c7
damcio <d.p.koziol@gmail.com>
2023-03-05 21:52:31 +0100

    Function newBookCreate added

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let addBookForm = document.getElementById("addBookForm");
 let newTitle = document.getElementById("inputTitle");
 let newAuthor = document.getElementById("inputAuthor");
 let newPages = document.getElementById("inputPages");
+let newRead = document.getElementById("inputRead");
 let submitBtn = document.getElementById("submitBtn");
 let resetBtn = document.getElementById("resetBtn");
 
@@ -26,3 +27,20 @@ function addBookToLibrary(title, author, pagesNum, haveRead) {
   const myBook = new Book(title, author, pagesNum, haveRead);
   myLibrary.push(myBook);
 }
+
+// Adding new book from form function
+
+function newBookCreate() {
+  const bookAdded = Object.create(Book, {
+    title: newTitle.value,
+    author: newAuthor.value,
+    pagesNum: newPages.value,
+    haveRead: newRead.value,
+  });
+  addBookToLibrary(bookAdded);
+  newBookCard();
+}
+
+// Creates new book obj and new card
+
+submitBtn.addEventListener("click", newBookCreate);

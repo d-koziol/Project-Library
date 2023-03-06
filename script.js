b544da079a067c44b78e36b006c8b53e4679ecff
damcio <d.p.koziol@gmail.com>
2023-03-06 21:58:24 +0100

    Function library added.

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ let newRead = document.getElementById("inputRead");
 let submitBtn = document.getElementById("submitBtn");
 let resetBtn = document.getElementById("resetBtn");
 
-addBookBtn.addEventListener("click", () => {
-  if (addBookForm) addBookForm.style.visibility = "visible";
-});
 // Book Class
 class Book {
   constructor(title, author, pagesNum, haveRead) {
@@ -26,17 +23,41 @@ class Book {
   }
 }
 
+// Table iterate and new book card create function
+
+function library() {
+  let libraryBook = document.querySelector(".libraryCards");
+  libraryBook.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    let book = myLibrary[i];
+    let bookCard = document.createElement("div");
+    bookCard.innerHTML = `
+    <div class="card">
+      <h2 class="title">${book.title}</h2>
+      <h3 class="author">${book.author}</h3>
+      <h3 class="pagesNum">${book.pagesNum}</h3>
+      <p class="haveRead">${book.haveRead ? "Read" : "Not Read Yet"}</p>
+    </div>
+    `;
+    libraryBook.appendChild(bookCard);
+  }
+}
+// Add new book function
+
 function addBookToLibrary() {
   let title = newTitle.value;
   let author = newAuthor.value;
   let pagesNum = newPages.value;
   let haveRead = newRead.checked;
-
   let myBook = new Book(title, author, pagesNum, haveRead);
   myLibrary.push(myBook);
-  console.log(myLibrary);
+  library();
 }
 
-// Creates new book obj and new card
+// Event listeners
 
 submitBtn.addEventListener("click", addBookToLibrary);
+
+addBookBtn.addEventListener("click", () => {
+  addBookForm.style.visibility = "visible";
+});
